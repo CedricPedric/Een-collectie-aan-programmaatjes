7dbad3c4df5d44abe3688b4d2ec7e7aeff2ee73e
CedricPedric <99059069@mydavinci.nl>
2021-10-01 22:21:39 +0200

    Uno Commit met Deck builder

---
+++ UNO.py
@@ -0,0 +1,29 @@
+import random
+
+
+#Maakt het Uno Deck
+def deckBuilder():
+    deck = []
+    kleuren = ['rood', 'groen', 'blauw', 'geel']
+    getalen = [0,1,2,3,4,5,6,7,7,8,9, 'Draw Two' ,'Skip Turn', 'Reverse']
+    wild = ['Wild', 'Wil Draw Four']
+
+    for kleur in kleuren:
+        for getal in getalen:
+            deck.append(str(kleur) + ' ' + str(getal))
+            if getal != 0:
+                deck.append(str(kleur) + ' ' + str(getal))
+        
+    for x in range(4):
+        deck.append(wild[0])
+        deck.append(wild[1])
+
+    return deck
+
+UnoDeck = deckBuilder()
+random.shuffle(UnoDeck)
+
+
+spelers = []
+amountSpelers = int(input("Vul aatal spelers in: "))
+

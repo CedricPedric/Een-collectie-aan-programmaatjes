d3e2da0c42ec1901ee6a9cf5876b3faa4043bb8b
CedricPedric <99059069@mydavinci.nl>
2021-10-05 20:47:02 +0200

    Yahtzee met bugfixes en changes to vars

--- yahtzee.py
+++ yahtzee.py
@@ -1,10 +1,23 @@
 import random
 
-Score =['',]*5
-Dobbels = ['',]*5
-resultaten = []
-
-
+scores = [''] *3
+rolledDice = ['']*5
+#Function voor dobbelstenen rerollen
+def reroll(givenRerolls:list):
+    global rolledDice
+    for x in (givenRerolls):
+        position = x -1
+        roll = random.randint(1,6)
+        rolledDice[position] = roll
+#Functon voor de dobbelstenen rollen
+def rollDice(begin=1,end = 5):
+    global rolledDice
+    while begin <= end:
+        position = begin - 1
+        roll = random.randint(1,6)
+        rolledDice[position]= roll
+        begin += 1
+#Function voor de text/resultaten van de dobbelstenen
 def text():
     TEXT = """
 1: Dobbelsteen = {}
@@ -12,24 +25,9 @@ def text():
 3: Dobbelsteen = {}
 4: Dobbelsteen = {}
 5: Dobbelsteen = {}
-    """.format(Dobbels[0], Dobbels[1], Dobbels[2], Dobbels[3],Dobbels[4])
+""".format(rolledDice[0], rolledDice[1], rolledDice[2], rolledDice[3],rolledDice[4])
     return TEXT
-
-def dobbel(begin=1,einde = 5):
-    global Dobbels
-    while begin <= einde:
-        positie = begin - 1
-        roll = random.randint(1,6)
-        Dobbels[positie]= roll
-        begin += 1
-
-def reroll(getalRerolls:list):
-    global Dobbels
-    for x in (getalRerolls):
-        positieVanKeuze = x -1
-        roll = random.randint(1,6)
-        Dobbels[positieVanKeuze] = roll
-
+#Simple Yes or No functie
 def YesOrNo(vraag = 'yes or no'):
     while True:
         antwoord = input(vraag)
@@ -39,96 +37,94 @@ def YesOrNo(vraag = 'yes or no'):
             return False
         else:
             print("Invalid Option!")
-
-def score(givenList):
-    global hoevaak
-    global Dobbels
-    finalLowResult = 0
-#Onder Berekening
-    for x in range(1,7):
-        y = x -1
-        hoevaak.append(givenList.count(x))
-        finalLowResult = finalLowResult + hoevaak[y]
-
-    TEXT = """
-Totaal aantal van 1: {0} 
-Totaal aantal van 2: {1} 
-Totaal aantal van 3: {2} 
-Totaal aantal van 4: {3} 
-Totaal aantal van 5: {4} 
-Totaal aantal van 6: {5} 
-Totaal alles opgeteld is: {6}
-    """.format(hoevaak[0],hoevaak[1],hoevaak[2],hoevaak[3],hoevaak[4],hoevaak[5],finalLowResult)
-
-#Boven Berekening
-    finalTopResult = 0
-    if 5 in hoevaak:
+#Funcite om de score te berkenen van het gegooide dobbelstenen
+def score(givenList:list):
+    #Checkt hoeveel van elk getal in de list staat en stopt dat in een list
+    howManyDigits = []
+    for digit in range(1,7):
+        howManyDigits.append(givenList.count(digit))
+    #Berekent de resultaten van bovenste helft
+    listUpperHalf = []
+    resultsUpperHalf = 0
+    for z in range(0,6):
+        x = howManyDigits[z] * (z + 1)
+        listUpperHalf.append(x)
+    for z in range(0,5):
+        resultsUpperHalf += givenList[z]
+    #Berekent de resultaten van de onderste helft/Bonus Points
+    bonusPoints = 0
+    bonus = ""
+    if 5 in howManyDigits:
         bonus = "Yahtzee!"
-        finalTopResult + 50
-    elif 1 in Dobbels and 2 in Dobbels and 3 in Dobbels and 4 in Dobbels and 5 in Dobbels:
-        finalTopResult + 30
-        bonus = "Kleine Straat"
-    elif 2 in Dobbels and 3 in Dobbels and 4 in Dobbels and 5 in Dobbels and 6 in Dobbels:
-        finalTopResult + 40
+        bonusPoints = 50
+    elif (1 in givenList and 2 in givenList and 3 in givenList and 4 in givenList and 5 in givenList) or (2 in givenList and 3 in givenList and 4 in givenList and 5 in givenList and 6 in givenList):
         bonus = "Grote Straat"
-    elif 4 in hoevaak:
-        for x in range(0,5):
-            y = x - 1
-            if givenList[y] == givenList[x]: 
-                finalTopResult = givenList[x] * 4
-                break
-        bonus = "Four of a kind!"
-    elif 3 in hoevaak and 2 in hoevaak:
-        finalTopResult = + 25
-        bonus = "Full house!"
-    elif 3 in hoevaak:
-        bonus = "Three of a kind!"
-        for x in range(0,5):
-            y = x - 1
-            if givenList[y] == givenList[x]: 
-                finalTopResult = givenList[x] * 3
-                break
-    
-#Alles optellen
-    FinalResult = finalLowResult + finalTopResult
-    print('-'*20)
-    print(hoevaak)
-    if finalTopResult > 0:
-        print(bonus)
-        print("U heeft een Bonus Score = " + str(finalTopResult))
-    print(TEXT)
-    print("Alles Opgeteld = ", str(FinalResult))
-    print('-'*20)
-    return FinalResult
+        bonusPoints = 40
+    elif (1 in givenList and 2 in givenList and 3 in givenList and 4 in givenList) or (2 in givenList and 3 in givenList and 4 in givenList and 5 in givenList) or (3 in givenList and 4 in givenList and 5 in givenList and 6 in givenList):
+        bonus = "Kleine Straat"
+        bonusPoints = 30
+    elif 4 in howManyDigits:
+        bonus = "Four of a Kind"
+        for positie in range(0,5):
+            bonusPoints += givenList[positie]
+    elif 3 in howManyDigits and 2 in howManyDigits:
+        bonus = "Full House!"
+        bonusPoints = 25
+    elif 3 in howManyDigits:
+        bonus = "Three of a Kind"
+        for positie in range(0,5):
+            bonusPoints += givenList[positie]
+    #Bereken resultaten
+    subTotal = bonusPoints + resultsUpperHalf
+    #Maakt en format de texten
+    TextBovensteHelft = """
+Totaal resultaten van allen 1's: {0} 
+Totaal resultaten van allen 2's: {1} 
+Totaal resultaten van allen 3's: {2} 
+Totaal resultaten van allen 4's: {3} 
+Totaal resultaten van allen 5's: {4} 
+Totaal resultaten van allen 6's: {5} 
+Totaal resultaten opgeteld is: {6}
+""".format(listUpperHalf[0],listUpperHalf[1],listUpperHalf[2],listUpperHalf[3],listUpperHalf[4],listUpperHalf[5],resultsUpperHalf)
 
+    TextOndersteHelft = """U heeft: {}
+Je extra punten zijn: {}
+""".format(bonus,bonusPoints)
 
+    #Prints naar de terminal
+    print('-'*40)
+    print(TextBovensteHelft)
+    if bonusPoints > 0:
+        print(TextOndersteHelft)
+    print("Het subtotaal is:", str(subTotal))         
+    print('-'*40)
+    #Als laaste wordt het subtotaal terug gegeven
+    return subTotal
 
-# Begin Code
-for game in range(1,6):
-    if YesOrNo("Dit is ronde {} wilt u een een ronde spelen?: ".format(game)):
-        dobbel()
+#De code voor de game
+for rondes in range(1,4):
+    if YesOrNo('Dit is ronde ' + str(rondes) +' wil je een nog een ronde?:'):
+        rollDice()
         print(text())
-        hoevaak = []
         for x in range(2):
-            if YesOrNo("Wilt u rerollen?:"):
-                rerolls = []
+            listRerolls = [] #Geeft hier aan dat de list leeg is zodat de oude rerolls niet worden gebruikt
+            if YesOrNo("Wilt u dobbelstenen rerollen?: "):
                 while True:
                     inputReroll = int(input("Welke dobbelstenen moeten opnieuw gerolled worden? (0 is done): "))
                     if inputReroll != 0:
-                        rerolls.append(inputReroll)
+                        listRerolls.append(inputReroll)
                     else:
                         break
-                reroll(rerolls)
-                print("New" + text())
-            else:
-                break
-        ScoreRound = (score(Dobbels))
-        positie = game - 1
-        Score[positie] = ScoreRound
+                reroll(listRerolls)
+                print("Rerolled Dice" + text())
+        ScoreThisRound = score(rolledDice)
+        postion = rondes - 1
+        scores[postion] = ScoreThisRound
     else:
-        print("Geen rondes meer")
         break
-
-print("Uw eind scores zijn:", Score)
-
+#De prints voor de finalscore
+print("""Score Round 1: {}
+Score Round 2: {}
+Score Round 3: {}
+""".format(scores[0],scores[1],scores[2]))
 
